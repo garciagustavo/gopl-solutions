db4cd1e00f5e2fd55088443733ce42d2f18ad552
Gustavo Garcia <ggarcpro@gmail.com>
2020-08-24 17:23:16 -0300

    Tutorial

---
+++ chapter01/exercise-1.3/main.go
@@ -0,0 +1,6 @@
+// Chapter 1 - Exercise 1.3
+
+// Text: Experiment to measure the difference in running time between our
+//       potentially inefficient versions and the one that uses strings.Join.
+
+

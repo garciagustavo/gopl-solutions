1bd6d1c94ba033f29f9497d377bbc1aedd1f3823
Gustavo Garcia <ggarcpro@gmail.com>
2020-08-24 17:20:09 -0300

    Print command line arguments and its indexex

---
+++ chapter01/exercise-1.2/main.go
@@ -0,0 +1,18 @@
+// Chapter 1 - Exercise 1.2
+
+// Text: Modify the echo program to print the index and value of each of its
+//       arguments, one per line.
+
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    for i, arg := range os.Args {
+        fmt.Println("index: ", i, "value: ", arg)
+    }
+
+}
